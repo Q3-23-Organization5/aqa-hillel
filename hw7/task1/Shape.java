11461e743637c2c7c0e05e20e0fc1196f35c8bf1
OlenaMaliuhina <omaliuhina@outlook.com>
2023-11-10 16:11:16 +0100

    homework7

---
+++ hw7/task1/Shape.java
@@ -0,0 +1,48 @@
+package task1;
+
+import java.util.Objects;
+
+public class Shape {
+    protected String color;
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    protected double calculateArea(){
+        return 0;
+    };
+    protected double calculatePerimeter(){
+        return 0;
+    };
+    protected void paint(){
+    };
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shape shape = (Shape) o;
+        super.equals(o);
+        return Objects.equals(color, shape.color);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(color);
+    }
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        Rectangle rectangle = new Rectangle();
+        Triangle triangle = new Triangle(5,9);
+
+        Shape[] shapesArray = {circle, triangle,rectangle};
+        for (Shape shape : shapesArray){
+            shape.calculateArea();
+            shape.paint();
+            System.out.println("ToString info: " + shape.toString());
+        }
+    }
+}
---
+++ hw7/task2/ProgrCalculator.java
@@ -0,0 +1,13 @@
+package task2;
+
+class ProgrCalculator extends Calculator {
+    public void calculateAverage(double[] values) {
+        for (int i = 0; i < values.length; i++) {
+            result = result+values[i];
+        }
+    }
+    @Override
+    public void displayResult() {
+        System.out.println("Result of ProgCalculator sum of array elements: " + result);
+    }
+}
